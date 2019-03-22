aef78d5f27dbe5dc01db771572ab482b193bfcb7
jacobpickett <48698415+jacobpickett@users.noreply.github.com>
2019-03-21 17:28:50 -0700

    added peanut butter fixed .cpp files

---
+++ Frank/TextAdventureVisualStudio/Source/PeanutButterFunctions.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+filename    PeanutButterFunctions.h
+author     Jacob Pickett
+
+Brief Description: builds the PeanutButter item
+
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+Item* PeanutButter_build();
+
+
+
+
+

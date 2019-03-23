c45e2b0970830a6e56f05e4c4b1fa3b4906ca177
tseringgg <47801677+tseringgg@users.noreply.github.com>
2019-03-22 17:37:12 -0700

    Recommit with CrunchyPeanutButterFunctions.c, RobotFunctions.c.

---
+++ Tsering/TextAdventureVisualStudio/RobotFunctions.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+filename    RobotFunctions.c
+author      Tsering Ngoche
+DP email    N/A
+course      GAM100 ** Do not use this code in your team project
+
+Brief Description:
+This file defines the functions to create a specific item, the "robot".
+
+******************************************************************************/
+#include "stdafx.h" /* UNREFERENCED_PARAMETER, NULL*/
+#include "RobotFunctions.h" /* Function declarations */
+#include "GameState.h" /* struct GameState, GameState_ChangeScore */
+#include "GameFlags.h" /* GameFlags_IsInList */
+#include "WorldData.h" /* WorldData_GetRoom */
+#include "Room.h" /* Room_GetItemList, Room_SetDescription */
+#include "ItemList.h" /* ItemList_FindItem, ItemList_Remove, ItemList_Add */
+#include "Item.h" /* Item_Create */
+#include "EggFunctions.h" /* Egg_Build */
+
+
+
+/* Build a "robot" object */
+Item* Robot_Build()
+{
+	/* Create a "robot" item, using the functions defined in this file */
+	return Item_Create("robot", "<add description>", false, NULL, NULL, NULL, NULL, NULL);
+}
